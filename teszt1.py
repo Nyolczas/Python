237ba23c2235a86beface6c42cd7e3e2621100ea
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-11-25 15:36:29 +0100

    teszt

--- teszt1.py
+++ teszt1.py
@@ -1,2 +1,7 @@
 x = 8
-print("hello" , str(x) + "-as" )
+print( "hello" , str(x) + "-as" )
+print( "7 is not x:", 7 is not x )
+print( "8 is x:", 8 is x)
+print( "8.0 is x:", 8.0 is x)
+print( "8.0 == x:", 8.0 == x)
+
