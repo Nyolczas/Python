92dd8e8417a75d984ccb918e4a2dd9aa8f6762ba
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-11-25 16:02:17 +0100

    teszt

--- teszt1.py
+++ teszt1.py
@@ -5,3 +5,12 @@ print( "8 is x:", 8 is x)
 print( "8.0 is x:", 8.0 is x)
 print( "8.0 == x:", 8.0 == x)
 
+def compare_to_8(y):
+    if y > 8:   
+        return str(y) + " nagyobb, mint 8"
+    elif y < 8:   
+        return str(y) + " kisebb, mint 8"
+    else:
+        return str(y) + " annyi, mint 8" 
+
+print(compare_to_8(7))        
