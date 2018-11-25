6c03b522ec5cbea47162e7cca156d4bd9c5035f0
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-11-25 18:17:19 +0100

    tanulás

--- teszt1.py
+++ teszt1.py
@@ -13,4 +13,24 @@ def compare_to_8(y):
     else:
         return str(y) + " annyi, mint 8" 
 
-print(compare_to_8(7))        
+print(compare_to_8(7))    
+
+lista = [10,20,30,40]
+print("max(lista)", max(lista) )
+print("min(lista)", min(lista) )
+print("sum(lista)", sum(lista) )
+print(round(13.276543, 2))
+
+print("lista:", lista )
+lista.sort(reverse = True)
+print("lista.sort(reverse = True):", lista )
+
+
+z = -20
+print(abs(z))
+print(pow(2,12))
+
+# tuples
+(years,profit) = "15,200%".split(",")
+print(years)
+print(profit)
