5c93d1fb39e6b8094aafef7211e1687aec6e77dd
Nyolczas <nyolczas@gmail.com>
2018-12-04 21:22:53 +0100

    adatműveletek

--- importigData_Quando.py
+++ importigData_Quando.py
@@ -1,4 +1,21 @@
 import quandl
 
 mydata_01 = quandl.get("FRED/GDP")
+print(mydata_01.head())
 print(mydata_01.tail())
+
+# adatok kiírása csv-be:
+import pandas as pd
+
+mydata_01.to_csv('C:/Users/User/Documents/GitHub/Python/csvData/usaGDP.csv')
+
+# adatok behívása csv-ből:
+mydata_02 = pd.read_csv('C:/Users/User/Documents/Tozsde/MT4ek/Admiral2/MQL4/Files/csvStatement_27018855/tickBalance.csv')
+print(mydata_02.head)
+print(mydata_02.tail)
+
+# a beolvasott csv kiírása excellbe:
+mydata_02.to_excel('C:/Users/User/Documents/GitHub/Python/csvData/tickBalance.xlsx')
+
+
+
