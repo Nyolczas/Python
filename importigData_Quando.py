f92ab6e1dd84c4c1ef8ac0f255bb6a244b19cef2
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-12-04 22:37:24 +0100

    adat beolvasások

--- importigData_Quando.py
+++ importigData_Quando.py
@@ -6,16 +6,17 @@ print(mydata_01.tail())
 
 # adatok kiírása csv-be:
 import pandas as pd
-
-mydata_01.to_csv('C:/Users/User/Documents/GitHub/Python/csvData/usaGDP.csv')
+import os
+mydata_01.to_csv(os.environ['USERPROFILE']+'/Documents/GitHub/Python/csvData/usaGDP.csv')
 
 # adatok behívása csv-ből:
-mydata_02 = pd.read_csv('C:/Users/User/Documents/Tozsde/MT4ek/Admiral2/MQL4/Files/csvStatement_27018855/tickBalance.csv')
+mydata_02 = pd.read_csv(os.environ['USERPROFILE']+'/Documents/Tozsde/MT4ek/Admiral2/MQL4/Files/csvStatement_27018855/tickBalance.csv')
 print(mydata_02.head)
 print(mydata_02.tail)
 
 # a beolvasott csv kiírása excellbe:
-mydata_02.to_excel('C:/Users/User/Documents/GitHub/Python/csvData/tickBalance.xlsx')
+mydata_02.to_excel(os.environ['USERPROFILE']+'/Documents/GitHub/Python/csvData/tickBalance.xlsx')
 
-
-
+#lehagyja a beolvasásból az alapértelmezett sorszámozást és az indexelő oszlop a megadott oszlop lesz 
+mydata_03 = pd.read_csv(os.environ['USERPROFILE']+'/Documents/GitHub/Python/csvData/Data_02.csv', index_col='Date')
+print(mydata_03)
