6b4c21982805a0e0e3195bc8de6be77d7695b298
Nyolczas <nyolczas@gmail.com>
2018-12-05 19:41:26 +0100

    az első chartok

--- simpleRateOfReturn.py
+++ simpleRateOfReturn.py
@@ -6,6 +6,18 @@ PG = wb.DataReader('PG', data_source='yahoo', start='1995-1-1')
 print(PG.head())
 print(PG.tail())
 
+#line chart
+PG['Adj Close'].plot(figsize=(16,9))
+plt.show()
+
 #napi %-os hozam számítása:
 PG['simple_return'] = (PG['Adj Close'] / PG['Adj Close'].shift(1))-1
 print(PG['simple_return'])
+
+PG['simple_return'].plot(figsize=(16,9))
+plt.show()
+
+avg_returns_d = PG['simple_return'].mean() # átlag számítása
+
+avg_returns_year = PG['simple_return'].mean() * 250
+print('éves átlag hozam: ' + str(round(avg_returns_year * 100, 2) ) + ' %')
