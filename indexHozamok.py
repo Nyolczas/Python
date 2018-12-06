5878a933d4f937962672ecbecca120940e1513b2
Nyolczas <nyolczas@gmail.com>
2018-12-06 21:25:42 +0100

    évesített hozamok

--- indexHozamok.py
+++ indexHozamok.py
@@ -10,4 +10,14 @@ ind_data = pd.DataFrame()
 for t in tickers:
     ind_data[t] = wb.DataReader(t, data_source='yahoo', start='1997-1-1')['Adj Close']
 
-print(ind_data.head())    
+print(ind_data.head())   
+
+ind_returns = (ind_data / ind_data.shift(1)) -1
+print(ind_returns.tail())
+
+annual_ind_returns = ind_returns.mean() * 250
+print(annual_ind_returns)
+
+(ind_data / ind_data.iloc[0] * 100).plot(figsize=(16, 9))
+plt.title("Évesített index hozam: " + str(round(annual_ind_returns * 100, 2)) + ' %')
+plt.show() 
