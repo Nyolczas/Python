fbd1d96f9e5d5e7d586058554a0c35a131d12de4
Nyolczas <nyolczas@gmail.com>
2018-12-08 19:34:08 +0100

    list ...

--- MEGA/basics/sliceList.py
+++ MEGA/basics/sliceList.py
@@ -6,3 +6,24 @@ print(myList[1])
 print(myList[:2])
 print(myList[-2:])
 
+
+napok = ["hétfő", "kedd", "szerda", "csütörtök", "péntek", "szombat", "vasárnap"]
+
+print(napok[0:5])
+print(napok[4])
+print(napok[4:6])
+print(napok[6])
+
+myList.remove(112)
+print(myList)
+
+myList.append("szopogatás")
+print(myList)
+
+myList.pop(1)
+print(myList)
+
+napok.sort()
+print(napok)
+
+
