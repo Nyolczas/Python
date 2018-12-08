3b164597c5c13f4f3142d21a95329736ef9183e5
Nyolczas <nyolczas@gmail.com>
2018-12-08 22:08:14 +0100

    tkinter

--- youtube/randomNumberApp.py
+++ youtube/randomNumberApp.py
@@ -3,16 +3,19 @@ import random
 
 window = tkinter.Tk()
 
+window.configure(width=800, height=450, background="white")
+
 def RandomNumber():
     myRandom = random.uniform(-6.9, 10.5)
     joslat.configure(text = "A legközelebbi tréded lehet, hogy "
-                        + str(round(myRandom,1)) + " % -ot fog hozni.")
+                        + str(round(myRandom,1)) + "% -ot fog hozni.")
     myButton.configure(text="Jósolj Még!")                        
 
 myTitle = tkinter.Label(window, text = "Tréder Varázsgömb", font = "Roboto 16")   
 myTitle.pack()
 
-joslat = tkinter.Label(window, font="Roboto 8 bold")
+joslat = tkinter.Label(window, font="Roboto 12 bold")
+joslat.configure(width=100, height=50)
 joslat.pack()
 
 myButton = tkinter.Button(window,text="Jósolj!", command=RandomNumber)
