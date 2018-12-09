8f6759cf0f4aded62afd78d7caf9451231bf7a97
Nyolczas <nyolczas@gmail.com>
2018-12-09 11:19:23 +0100

    Update readTxt.py

--- MEGA/basics/readTxt.py
+++ MEGA/basics/readTxt.py
@@ -3,4 +3,14 @@ import os
 myfile = open(os.environ['USERPROFILE']+"/Documents/GitHub/Python/MEGA/basics/fruits.txt")
 fruits = myfile.read()
 myfile.close()
+
 print(fruits)
+print(type(fruits))
+print(len(fruits))
+print(fruits[0])
+
+fruits.splitlines()
+print(fruits)
+print(type(fruits))
+print(len(fruits))
+print(fruits[0])
