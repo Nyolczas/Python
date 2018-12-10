6b90868c97e4c2cb478960e2d56c8735f2a49fa9
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-12-10 16:58:26 +0100

    pie chart

--- MEGA/basics/readTxt.py
+++ MEGA/basics/readTxt.py
@@ -2,10 +2,10 @@ import os
 
 #myfile = open(os.environ['USERPROFILE']+"/Documents/GitHub/Python/MEGA/basics/fruits.txt")
 myfile = open("D:/8I/Code/Python/MEGA/basics/fruits.txt")
-fruits = myfile.read()
+content = myfile.read()
 myfile.close()
 
-fruits = fruits.splitlines()
+content = content.splitlines()
 
-for i in fruits:
+for i in content:
     print(i +": " + str(len(i)))
