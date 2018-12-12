51a844ed626b1a11e62f5f475a77cf1298592301
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-12-12 12:35:38 +0100

    label

--- youtube/tkinter_gyak02.py
+++ youtube/tkinter_gyak02.py
@@ -1,13 +1,25 @@
 from tkinter import*
 import os
 
+bgClr = '#4D4D4D' 
+txtClr = '#CCCCCC'
+
+# ablak osztály
 class Root(Tk):
     def __init__(self):
         super(Root, self).__init__()
         self.title('Menő Elemző Mező')
         self.minsize(800, 450)
         self.wm_iconbitmap(os.environ['USERPROFILE']+'/Documents/GitHub/Python/youtube/RetroBlock.ico')
-        self.configure(background = '#4D4D4D')
+        self.configure(background = bgClr)
+        self.createWidget()
+
+    # label  
+    def createWidget(self):     
+        label = Label(self, text = 'Ez az analizálandó téma neve')
+        label.grid(column = 0, row = 0)
+        label.configure(foreground = txtClr, background = bgClr)
+      
 
 root = Root()
 root.mainloop()        
