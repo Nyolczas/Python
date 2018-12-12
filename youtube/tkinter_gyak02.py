7ae4ed966f61281bc8b7c00d068534e6588b5574
Nyolczas <nyolczas@gmail.com>
2018-12-12 13:15:10 +0100

    button command

--- youtube/tkinter_gyak02.py
+++ youtube/tkinter_gyak02.py
@@ -1,8 +1,10 @@
-from tkinter import*
 import os
+from tkinter import ttk
+from tkinter import*
 
 bgClr = '#4D4D4D' 
 txtClr = '#CCCCCC'
+hiClr = 'orange'
 
 # ablak osztály
 class Root(Tk):
@@ -14,11 +16,25 @@ class Root(Tk):
         self.configure(background = bgClr)
         self.createWidget()
 
+         # button
+        self.button = ttk.Button(self, text ="Click me!", command = self.btnClick)    
+        self.button.grid(column = 0, row = 1)  
+    
+        #button label
+        self.btnLabel = ttk.Label(self, text = "Hello Tkinter")
+        self.btnLabel.grid(column = 1, row = 1)
+        self.btnLabel.configure(foreground = txtClr, background = bgClr)
+
     # label  
     def createWidget(self):     
-        label = Label(self, text = 'Ez az analizálandó téma neve')
+        label = Label(self, text = 'Ez az analizálandó téma címe') #  , font="Roboto 12 bold"
         label.grid(column = 0, row = 0)
         label.configure(foreground = txtClr, background = bgClr)
+
+    def btnClick(self):
+        self.btnLabel.configure(text = "most ideklikkoltál és megváltoztattad a szöveget")
+        self.btnLabel.configure(foreground = hiClr)
+
       
 
 root = Root()
