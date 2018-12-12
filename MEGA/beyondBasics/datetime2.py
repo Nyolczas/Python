b05eb1de0c0c979b805d45e6681a75fd4cf516a7
Nyolczas <nyolczas@gmail.com>
2018-12-12 17:35:19 +0100

    datetime jav

--- MEGA/beyondBasics/datetime2.py
+++ MEGA/beyondBasics/datetime2.py
@@ -1,3 +1,4 @@
 from datetime import datetime
+
 delta = datetime.now() - datetime(1975, 7, 2)
 print(delta.days)
--- MEGA/beyondBasics/zip.py
+++ MEGA/beyondBasics/zip.py
@@ -2,4 +2,7 @@ a = ['a', 'b', 'c']
 b = [1, 2, 3]
 
 for i, j in zip(a, b):
-    print("%s is %s" %(i, j))
+    print("%s is %s" %(i, j)) # string formatted version
+
+for i, j in zip(a, b):
+    print(i, j) # simple version
