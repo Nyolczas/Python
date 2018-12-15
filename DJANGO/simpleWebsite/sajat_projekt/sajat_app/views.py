4af3d9f2b01fa2dd0710744346db96cb61cb8db7
Nyolczas <nyolczas@gmail.com>
2018-12-15 20:34:18 +0100

    müxik

--- DJANGO/simpleWebsite/sajat_projekt/sajat_app/views.py
+++ DJANGO/simpleWebsite/sajat_projekt/sajat_app/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello World!")
+    my_dictionary = {'insert_me' : "Hello én már a pythonból jöttem!"}
+    #ez küldi vissza
+    return render(request, 'sajat_app/index.html',context=my_dictionary)
+
+def help(request):
+    helpdict = {'help_content' : 'Ez a segítség már a PYTHONBÓL gyütt!'}
+    return render(request,'sajat_app/help.html',context=helpdict)  
