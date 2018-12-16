7a22b3965c0bac8d156a95de42e4818bb67428c0
Nyolczas <nyolczas@gmail.com>
2018-12-16 17:03:27 +0100

    level2 és jegyzetek

--- DJANGO/simpleWebsite/level2_project/level2_project/urls.py
+++ DJANGO/simpleWebsite/level2_project/level2_project/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from level2_app import views
 
 urlpatterns = [
+    path('', views.index, name="index"),
+    path('level2_app/', include('level2_app.urls')),
     path('admin/', admin.site.urls),
 ]
